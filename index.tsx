c358491f6fb6be9637df2a8d03123516a176c4a8
Evan Sherwood <evan@sherwood.io>
2018-05-25 20:38:09 -0700

    refactor: inline View

--- index.tsx
+++ index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import md5 from 'md5-o-matic'
-import View from './View'
 
 interface FinalOptionProps {
   readonly innerRef: (el: HTMLElement) => void
@@ -224,7 +223,25 @@ class Menu extends React.Component<Props, State> {
       })
     }
 
-    return <View {...props} />
+    return (
+      <React.Fragment>
+        <button {...props.controlProps}>
+          {props.label({ isOpen: props.isOpen })}
+        </button>
+        {props.isOpen && (
+          <ul
+            role='menu'
+            aria-labelledby={props.controlProps.id}
+          >
+            {props.options.map(([id, option]) => (
+              <li role='presentation' key={id}>
+                {option}
+              </li>
+            ))}
+          </ul>
+        )}
+      </React.Fragment>
+    )
   }
 }
 
