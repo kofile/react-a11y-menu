1d94d5336304226f087d0fc652cc542f47f992c9
Evan Sherwood <evan@sherwood.io>
2018-05-25 22:15:41 -0700

    feat: render options via props
    Also keep track of selected index outside of react state, and only iterate over
    tabbable elements
    
    BREAKING CHANGE: completely new public API, so not at all backwards compatible

--- index.tsx
+++ index.tsx
@@ -1,119 +1,51 @@
 import * as React from 'react'
-import md5 from 'md5-o-matic'
 import FocusTrap from 'focus-trap-react'
+import tabbable from 'tabbable'
 
-interface FinalOptionProps {
-  readonly innerRef: (el: HTMLElement) => void
-  readonly id: string
-  readonly role: string
-  readonly tabIndex: number
-  readonly onClick?: () => void
-  readonly onKeyDown: (e: React.KeyboardEvent<Event>) => void
+interface ContainerProps {
+  readonly role: 'menu'
+  readonly 'aria-labelledby': string
 }
 
-type OptionProps = [string, FinalOptionProps, React.ReactNode]
-type Options = Array<OptionProps>
-type Ref = React.RefObject<HTMLElement>
-type RecordRef = (ref: Ref) => void
-type OnClick = () => void
-type HandleMenuButtonKeys = (event: React.KeyboardEvent<HTMLButtonElement>) => void
-type KeyboardEvent = (event: React.KeyboardEvent<Event>) => void
-type HandleMenuKeys = (onClickHandler: OnClick) => KeyboardEvent
-
-interface GenerateOption {
-  prefix: string
-  recordRef: RecordRef
-  onKeyDown: HandleMenuKeys
-  close: () => void
+interface ItemProps {
+  readonly role: 'menuitem'
+  readonly onKeyDown: (event: React.KeyboardEvent<Event>) => void
 }
 
-interface GenerateOptions extends GenerateOption {
-  options: Options
+interface RenderOptionsProps {
+  readonly containerProps: ContainerProps
+  readonly itemProps: ItemProps
 }
 
-export interface ViewProps {
+interface RenderLabelProps {
   readonly isOpen: boolean
-  readonly label: ({ isOpen }: { isOpen: boolean }) => React.ReactNode
-  readonly options: Array<[string, React.ReactNode]>
-  readonly controlProps: {
-    readonly 'aria-haspopup': 'menu'
-    readonly onClick: OnClick
-    readonly 'aria-expanded': boolean
-    readonly id: string
-    readonly onKeyDown: HandleMenuButtonKeys
-    readonly ref: React.RefObject<HTMLButtonElement>
-    readonly className?: string
-  }
 }
 
 interface Props {
   readonly id: string
-  readonly label: ({ isOpen }: { isOpen: boolean }) => React.ReactNode
-  readonly options: Options
+  readonly renderLabel: (opts: RenderLabelProps) => React.ReactNode
+  readonly renderOptions: (opts: RenderOptionsProps) => React.ReactNode
   readonly className?: string
 }
 
 interface State {
   isOpen: boolean
-  menuItemFocusIndex: number
 }
 
 class Menu extends React.Component<Props, State> {
-  menuItemRefs: Array<React.RefObject<HTMLElement>> = []
-  controlRef: React.RefObject<HTMLButtonElement> = React.createRef()
+  tabbableElems: Array<HTMLElement> = []
+  selectedIndex: number = 0
+  controlRef: React.RefObject<HTMLButtonElement> = React.createRef<HTMLButtonElement>()
 
   state = {
-    isOpen: false,
-    menuItemFocusIndex: 0
+    isOpen: false
   }
 
-  constructor(props: Props) {
-    super(props)
+  open = () => this.setState({ isOpen: true })
+  close = () => this.setState({ isOpen: false })
 
-    /**
-     * We rely on options being unique amongst their peers in order to keep
-     * track of the automatically generated refs
-     */
-    checkForDuplicateOptions(props.options)
-  }
-
-  static getDerivedStateFromProps(props: Props, state: State) {
-    checkForDuplicateOptions(props.options)
-
-    return {
-      isOpen: state.isOpen,
-      menuItemFocusIndex: 0
-    }
-  }
-
-  /**
-   * Handler for opening the menu, which sets the menu state to open and
-   * automatically focuses the first menu option.
-   */
-  open = () => {
-    this.setState({ isOpen: true, menuItemFocusIndex: 0 })
-  }
-
-  /**
-   * Handler for closing the menu. Clears out our dynamic ref tracking.
-   *
-   * NOTE: When migrating to the new createRef API, this "clearing" step will
-   * possibly become unnecessary.
-   */
-  close = () => {
-    this.setState({ isOpen: false, menuItemFocusIndex: 0 })
-    this.menuItemRefs = []
-  }
-
-  refMenuItem: RecordRef = ref => {
-    this.menuItemRefs.push(ref)
-  }
-
-  /**
-   * Handler for keyboard actions on the menu control
-   */
-  handleMenuButtonKeys: HandleMenuButtonKeys = event => {
-    const upperIndex = this.props.options.length - 1
+  handleMenuButtonKeys = (event: React.KeyboardEvent<HTMLButtonElement>) => {
+    const upperIndex = this.tabbableElems.length - 1
 
     switch (event.key) {
       case 'Enter':
@@ -131,121 +63,82 @@ class Menu extends React.Component<Props, State> {
         event.preventDefault()
 
         if (!this.state.isOpen) {
-          this.setState({ isOpen: true, menuItemFocusIndex: upperIndex })
+          this.setState({ isOpen: true })
         }
 
         break
     }
   }
 
-  /**
-   * Handle keyboard navigation of menu items
-   */
-  handleMenuKeys: HandleMenuKeys = onClickHandler => event => {
-    const { menuItemFocusIndex } = this.state
-    const upperIndex = this.props.options.length - 1
+  handleMenuKeys = (event: React.KeyboardEvent<Event>) => {
+    const upperIndex = this.tabbableElems.length - 1
 
     switch (event.key) {
       case 'Escape':
         event.preventDefault()
-        this.close()
+        close()
         break
 
       case 'ArrowDown':
         event.preventDefault()
-
-        this.setState({
-          menuItemFocusIndex: advanceIndex(menuItemFocusIndex, upperIndex)
-        })
-
+        this.selectedIndex = advanceIndex(this.selectedIndex, upperIndex)
         break
 
       case 'ArrowUp':
         event.preventDefault()
-
-        this.setState({
-          menuItemFocusIndex: retreatIndex(menuItemFocusIndex, upperIndex)
-        })
-
+        this.selectedIndex = retreatIndex(this.selectedIndex, upperIndex)
         break
 
       case 'Home':
         event.preventDefault()
-        this.setState({ menuItemFocusIndex: 0 })
+        this.selectedIndex = 0
         break
 
       case 'End':
         event.preventDefault()
-        this.setState({ menuItemFocusIndex: upperIndex })
-        break
-
-      case 'Enter':
-        event.preventDefault()
-        event.stopPropagation()
-        onClickHandler()
+        this.selectedIndex = upperIndex
         break
     }
+
+    this.tabbableElems[this.selectedIndex].focus()
   }
 
   componentDidUpdate() {
-    const { menuItemFocusIndex, isOpen } = this.state
-
-    this.menuItemRefs = this.menuItemRefs.filter(r => !!r.current)
-
-    if (isOpen) {
-      const { current } = this.menuItemRefs[menuItemFocusIndex]
-
-      if (current) {
-        current.focus()
-      }
-    } else if (this.controlRef.current) {
-      this.controlRef.current.focus()
-    }
+    this.tabbableElems = tabbable(this.controlRef.current)
   }
 
   render() {
     const { isOpen } = this.state
+    const { id, className, renderLabel, renderOptions } = this.props
 
     const controlProps = {
+      id,
+      className,
       'aria-haspopup': 'menu' as 'menu',
       onClick: isOpen ? this.close : this.open,
       'aria-expanded': !!isOpen,
-      id: this.props.id,
       onKeyDown: this.handleMenuButtonKeys,
-      ref: this.controlRef,
-      className: this.props.className
+      ref: this.controlRef
     }
 
-    const props = {
-      isOpen: isOpen || false,
-      controlProps,
-      label: this.props.label,
-      options: generateOptions({
-        prefix: this.props.id,
-        options: this.props.options,
-        recordRef: this.refMenuItem,
-        onKeyDown: this.handleMenuKeys,
-        close: this.close
-      })
+    const containerProps = {
+      role: 'menu' as 'menu',
+      'aria-labelledby': id
+    }
+
+    const itemProps = {
+      onKeyDown: this.handleMenuKeys,
+      role: 'menuitem' as 'menuitem'
     }
 
     return (
       <React.Fragment>
-        <button {...props.controlProps}>
-          {props.label({ isOpen: props.isOpen })}
+        <button {...controlProps}>
+          {renderLabel({ isOpen })}
         </button>
-        {props.isOpen && (
-          <FocusTrap>
-            <ul
-              role='menu'
-              aria-labelledby={props.controlProps.id}
-            >
-              {props.options.map(([id, option]) => (
-                <li role='presentation' key={id}>
-                  {option}
-                </li>
-              ))}
-            </ul>
+        {isOpen && (
+          <FocusTrap focusTrapOptions={{ clickOutsideDeactivates: true }}>
+            {renderOptions({ containerProps, itemProps })}
           </FocusTrap>
         )}
       </React.Fragment>
@@ -254,20 +147,6 @@ class Menu extends React.Component<Props, State> {
 }
 
 export default Menu
-
-function hasDuplicates(options: Options) {
-  const optionsSet = new Set()
-
-  options.forEach(option => optionsSet.add(JSON.stringify(option)))
-
-  return options.length !== optionsSet.size
-}
-
-function checkForDuplicateOptions(options: Options) {
-  if (hasDuplicates(options)) {
-    throw new Error('Each menu option must be unique')
-  }
-}
 
 function advanceIndex(index: number, upperIndex: number) {
   const newIndex = index + 1
@@ -291,46 +170,4 @@ function retreatIndex(index: number, upperIndex: number) {
   }
 
   return newIndex
-}
-
-function generateOptions({ prefix, options, recordRef, onKeyDown, close }: GenerateOptions) {
-  return options.map(generateOption({ prefix, recordRef, onKeyDown, close }))
-}
-
-function generateOption({ prefix, recordRef, onKeyDown, close }: GenerateOption) {
-  return (optionProps: OptionProps): [string, React.ReactNode] => {
-    const [element, userProps, children] = optionProps
-    const id = `${prefix}--${md5(JSON.stringify(optionProps))}`
-    const ref = React.createRef<HTMLElement>()
-
-    recordRef(ref)
-
-    const doUserAction = () => {
-      close()
-
-      if (userProps.onClick) {
-        userProps.onClick()
-      }
-    }
-
-    const props = {
-      ...userProps,
-      onClick: (event: React.SyntheticEvent<MouseEvent>) => {
-        doUserAction()
-      },
-      id,
-      role: 'menuitem'
-    }
-
-    const finalProps =
-      typeof element === 'string'
-        ? { ...props, ref }
-        : { ...props, innerRef: ref }
-
-    finalProps.onKeyDown = onKeyDown(() => {
-      doUserAction()
-    })
-
-    return [id, React.createElement(element, finalProps, children)]
-  }
 }
